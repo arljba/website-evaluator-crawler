0b39f667adc1986542396c59eff0579885216036
arljba <33658992+arljba@users.noreply.github.com>
2020-09-08 13:41:03 +0200

    Fix code to work with srapyrt and http requests

--- crawler/spiders/LinkSpider.py
+++ crawler/spiders/LinkSpider.py
@@ -2,55 +2,53 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider
 from ..items import WebCheckScraperItem
+from urllib.parse import urlparse
+from scrapy.selector import Selector
 
 
 class LinkSpider(CrawlSpider):
     # The name of the spider
     name = "linkspider"
 
-    # only check internal domain links
-    allowed_domains = ["books.toscrape.com"]
-    # entry point
-    start_urls = ["http://books.toscrape.com/"]
-
+    # amount of pages that are being crawled. Keep it low to be considerate its just as a proof of concept.
     COUNT_MAX = 5
     custom_settings = {
         'CLOSESPIDER_PAGECOUNT': COUNT_MAX
     }
 
+    # scrapyrt (the webserver) doesnt allow params like domain ur starturl to be passed. Therefor this is a hacky way to extract params from the url.
+    # this fix follows the instructions given in (https://github.com/scrapinghub/scrapyrt/issues/29)
+    __allowedDomain = ("domain")
+    __allowedUrls = ("starturl")
+    domain = None
+    starturl = None
+
+    def __init__(self, domain=None, starturl=None, * args, **kwargs):
+        super(LinkSpider, self).__init__(*args, **kwargs)
+        for k, v in kwargs.items():
+            assert(k in self.__class__.__allowedDomain)
+            setattr(self, k, v)
+            assert(k in self.__class__.__allowedUrls)
+            setattr(self, k, v)
+        self.allowed_domains = [domain]
+        self.start_urls = [starturl]
+        print(self.allowed_domains)
+        print(self.start_urls)
+
     rules = [
         Rule(
             LinkExtractor(
-                canonicalize=True,
                 unique=True
             ),
             follow=True,  # recursive call
             callback="parse_items"
-        )
+        ),
     ]
-
-    def parse(self, response):
-        for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
-        for item in self.parse_items(response):
-            yield item
 
     # Method for parsing items
 
     def parse_items(self, response):
-        # liste of founf links
-        items = []
-        # only canonical and unique links
-        links = LinkExtractor(
-            canonicalize=True, unique=True).extract_links(response)
-        for link in links:
-            is_allowed = False
-            for allowed_domain in self.allowed_domains:
-                if allowed_domain in link.url:
-                    is_allowed = True
-            if is_allowed:
-                item = WebCheckScraperItem()
-                item['url_src'] = response.url
-                item['url_dest'] = link.url
-                items.append(item)
-        return items
+        item = WebCheckScraperItem()
+        item['url_src'] = response.request.headers.get('Referer').decode()
+        item['url_dest'] = response.url
+        yield item
